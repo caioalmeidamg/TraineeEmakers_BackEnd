9916dd7a6799bd325e493224d23df4802206af63
Caio Almeida Santos <aceutoxzc@gmail.com>
2023-07-07 02:58:57 -0300

    Tabela Pessoas criada e teste de migrations

---
+++ API/app/Controllers/Http/PessoasController.ts
@@ -0,0 +1,17 @@
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import Pessoa from 'App/Models/Pessoa'
+
+export default class PessoasController {
+    public async store({request, response}: HttpContextContract){
+       const body = request.body()
+
+       const pessoa = await Pessoa.create(body)
+
+       response.status(201);
+       
+       return{
+        message: "deu bom",
+        data: pessoa,
+       }
+    }
+}
